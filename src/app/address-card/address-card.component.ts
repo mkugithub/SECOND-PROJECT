f1e532e3259dca1bb1dc1bdee2ee987f3ad88e1d
MrityunjayKumarUpadhyay <mkuupadhyay123@gmail.com>
2023-05-22 14:02:35 +0530

    Styling Angular Component and Handling click events

--- src/app/address-card/address-card.component.ts
+++ src/app/address-card/address-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { User } from './user.model';
 
 @Component({
   selector: 'app-address-card',
@@ -6,26 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./address-card.component.css']
 })
 export class AddressCardComponent implements OnInit{
-  
-  user:any;
-constructor(){
-  this.user={
-    name:'Mrityunjay Upadhyay',
-        title:'Software Developer',
-        address:'1234 Main St, City, State, 100010',
-    
-    phone:[
-      '123-123-123-1234',
-      '456-456-456',
-      '567-897-543-432'
-    ] 
-    
-    }
-}
+@Input('user') user:User;
+isCollapsed: boolean=true;
 
+constructor(){
+
+}
+toggleCollapse(){
+  this.isCollapsed=!this.isCollapsed;
+}
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
-
   
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import{User} from './address-card/user.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  user:User;
+    
+  constructor(){
+   this.user=new User();
+   this.user.name="Foo Bar";
+   this.user.designation="Software Developer";
+   this.user.address="100 Street Napervile Cheicga"
+   this.user.phone=[
+    "132-123-1234",
+    "234-234-2345"
+   
+   ]
+
+
+  }
 }
